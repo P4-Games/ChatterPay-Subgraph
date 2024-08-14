460f8e4fee3ae6b01e2d3c9fa5063404ad47478b
Tomas <tomasfreire@gmail.com>
2024-08-13 21:14:30 -0300

    first commit

--- src/chatter-pay-wallet-factory.ts
+++ src/chatter-pay-wallet-factory.ts
@@ -1,27 +1,10 @@
 import {
-  OwnershipTransferred as OwnershipTransferredEvent,
   ProxyCreated as ProxyCreatedEvent
 } from "../generated/ChatterPayWalletFactory/ChatterPayWalletFactory"
-import { OwnershipTransferred, ProxyCreated } from "../generated/schema"
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import { Wallet } from "../generated/schema"
 
 export function handleProxyCreated(event: ProxyCreatedEvent): void {
-  let entity = new ProxyCreated(
+  let entity = new Wallet(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.owner = event.params.owner
